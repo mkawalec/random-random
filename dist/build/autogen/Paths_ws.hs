64dd0b791b1b57c7d91cfc62e1598e4b71c181c6
Michal Kawalec <michal@bazzle.me>
2015-03-05 19:09:32 +0100

    added some files

---
+++ dist/build/autogen/Paths_ws.hs
@@ -0,0 +1,35 @@
+module Paths_ws (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/tmp/ws/.cabal-sandbox/bin"
+libdir     = "/tmp/ws/.cabal-sandbox/lib/x86_64-linux-ghc-7.8.4/ws-0.1.0.0"
+datadir    = "/tmp/ws/.cabal-sandbox/share/x86_64-linux-ghc-7.8.4/ws-0.1.0.0"
+libexecdir = "/tmp/ws/.cabal-sandbox/libexec"
+sysconfdir = "/tmp/ws/.cabal-sandbox/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "ws_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "ws_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "ws_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "ws_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "ws_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
---
+++ src/Main.hs
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+    ( main
+    ) where
+
+
+--------------------------------------------------------------------------------
+import           Control.Concurrent  (forkIO)
+import           Control.Monad       (forever, unless)
+import           Control.Monad.Trans (liftIO)
+import           Network.Socket      (withSocketsDo)
+import           Data.Text           (Text)
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as T
+import qualified Network.WebSockets  as WS
+
+import qualified Data.CaseInsensitive as CI
+import           Data.ByteString (ByteString)
+import           Data.ByteString.Char8 (pack)
+import           Data.Time.Clock.POSIX (getPOSIXTime)
+import           System.IO.Unsafe (unsafePerformIO)
+
+import Network.WebSockets.Connection
+
+getTime :: IO Integer
+getTime = getPOSIXTime >>= \t -> return (floor (toRational t))
+
+twitterHeader :: ByteString
+twitterHeader = pack ("OAuth oauth_consumer_key=\"ytDrsZb2llyVvfbKOrcZbyA99\", oauth_nonce=\"bd57cfb9ff93b1a6d13e7eeb6ec5f099\", oauth_signature=\"%2Biw7YyWD8h0UEIa0O7Kx4aRvlh8%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"" + time  + "\", oauth_version=\"1.0\"")
+    where time = show $ unsafePerformIO getTime
+
+headers :: WS.Headers
+headers = [(CI.mk $ ((pack "Authorization")::ByteString), twitterHeader)]
+
+--------------------------------------------------------------------------------
+app :: WS.ClientApp ()
+app conn = do
+    putStrLn "Connected!"
+
+    -- Fork a thread that writes WS data to stdout
+    _ <- forkIO $ forever $ do
+        msg <- WS.receiveData conn
+        liftIO $ T.putStrLn msg
+
+    -- Read from stdin and write to WS
+    let loop = do
+            line <- T.getLine
+            unless (T.null line) $ WS.sendTextData conn line >> loop
+
+    loop
+    WS.sendClose conn ("Bye!" :: Text)
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = withSocketsDo $ WS.runClientWith "stream.twitter.com" 443 
+                                        "/1.1/statuses/sample.json" 
+                                        defaultConnectionOptions headers app
